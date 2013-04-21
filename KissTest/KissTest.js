65e5c0d703e54ad57aab0611967267d03fd70188
Nuno Oliveira <nmoliveira@gmail.com>
2013-04-21 01:07:34 +0100

    added comments

--- KissTest/KissTest.js
+++ KissTest/KissTest.js
@@ -1,4 +1,9 @@
+// By: Nuno Oliveira
+// https://github.com/nmoliveira/KissTestjs
+
+// kt provides the namespace
 var kt = function() {
+  // Creates/Get ul element for results list
   var getResultsList = function() {
     var results = document.getElementById('testResults');
     if(!results){
@@ -8,11 +13,18 @@ var kt = function() {
     }
     return results;
   };
+  // Tests if value is true
+  // @value - the test
+  // @desc - description of the test
   var assert = function(value, desc) {
+    // get list element
     var results = getResultsList();
+    // create new item for the list
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(desc));
+    // add class according to the result of the test 
     li.className = value ? 'kt-passed' : 'kt-failed';
+    // add item to list
     results.appendChild(li);
   };
   return {
