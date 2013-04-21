079ac7b845f528983d2e772f93b935ecebbe1180
Nuno Oliveira <nmoliveira@gmail.com>
2013-04-21 18:49:48 +0100

    added isStrictEqual method

--- KissTest/KissTest.js
+++ KissTest/KissTest.js
@@ -4,6 +4,29 @@
 // kt provides the namespace
 var kt = function() {
 
+  // Tests if value is true
+  // @value the test
+  // @desc description of the test
+  var assert = function(value, desc) {
+    kissTest(value, null, null, desc);
+  };
+
+  // Tests if @actual and @expected are equal
+  // @actual current value
+  // @expected expected value
+  // @desc description of the test
+  var isEqual = function(actual, expected, desc){
+    kissTest(actual == expected, actual, expected, desc)
+  };
+
+  // Tests if @actual and @expected are strict equal
+  // @actual current value
+  // @expected expected value
+  // @desc description of the test
+  var isStrictEqual = function(actual, expected, desc) {
+    kissTest(actual === expected, actual, expected, desc);
+  };
+
   // Creates/Get ul element for results list
   var getResultsList = function() {
     var results = document.getElementById('testResults');
@@ -15,37 +38,35 @@ var kt = function() {
     return results;
   };
 
-  // Tests if value is true
-  // @value the test
-  // @desc description of the test
-  var assert = function(value, desc) {
+  // main function
+  // @result the test to run
+  // @actual the current value
+  // @expected the expected value
+  // @desc description of the test 
+  var kissTest = function(result, actual, expected, desc){
     // get list element
     var results = getResultsList();
     // create new item for the list
     var li = document.createElement('li');
-    li.appendChild(document.createTextNode(desc));
-    // add class according to the result of the test 
-    li.className = value ? 'kt-passed' : 'kt-failed';
-    // add item to list
-    results.appendChild(li);
-  };
-
-  // Tests if @actual and @expected are equal
-  // @actual current value
-  // @expected expected value
-  // @desc description of the test
-  var isEqual = function(actual, expected, desc){
-    // get list element
-    var results = getResultsList();
-    // create new item for the list
-    var li = document.createElement('li');
-    // is actual equals to expected
-    if(actual == expected) {
+    // is test passed
+    if(result) {
+      // add test output
       var desc = document.createTextNode(desc);
+      // set class to the li element
       li.className = 'kt-passed';  
     }
-    else{
-      var desc = document.createTextNode(desc + ' -> Expected: "' + expected + '" Result : "' + actual + '"');
+    else{ 
+      // if test failed
+      // if actual and expected are filled
+      if (actual && expected) {
+        // add test output
+        var desc = document.createTextNode(desc + ' -> Expected: "' + expected + '" Result : "' + actual + '"');
+      }
+      else{
+        // add test output
+        var desc = document.createTextNode(desc);
+      }
+      // set class to the li element
       li.className = 'kt-failed';
     }
     li.appendChild(desc);
@@ -56,6 +77,7 @@ var kt = function() {
   // public region
   return {
     assert: assert,
-    isEqual: isEqual
+    isEqual: isEqual,
+    isStrictEqual: isStrictEqual
   }
 }();
